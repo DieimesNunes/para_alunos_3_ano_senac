fd206cda53c6e45f77c881c6e7cf515bb56a9797
Dieimes Nunes <dieimes.nunes@docente.pr.senac.br>
2023-05-08 06:29:30 -0300

    Exemplo

---
+++ teste.dart
@@ -0,0 +1,20 @@
+void main() {
+  List<String> alunos = [
+    "Marcelo",
+    "Joao",
+    "Alexia",
+    "Ana Maria",
+  ];
+
+  print(alunos);
+
+  List<int> idades = [
+    15,
+    16,
+    17,
+    18,
+  ];
+
+  idades.add(22); // Apara adicionar um elemento
+  print(idades);
+}
